83eed80f9b192ccf4aa3b820e181150d5d62d351
Nipun_105572 <nipun.thilakshan@axiatadigitallabs.com>
2021-07-15 21:30:54 +0530

    Add initial project files

---
+++ distributed-content-searching/src/main/java/com/dc/distributed/content/searching/DistributedContentSearchingApplication.java
@@ -0,0 +1,8 @@
+package com.dc.distributed.content.searching;
+
+public class DistributedContentSearchingApplication {
+
+	public static void main(String[] args) {
+	}
+
+}
---
+++ distributed-content-searching/src/test/java/com/dc/distributed/content/searching/DistributedContentSearchingApplicationTests.java
@@ -0,0 +1,5 @@
+package com.dc.distributed.content.searching;
+
+class DistributedContentSearchingApplicationTests {
+
+}
